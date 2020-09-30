a9b3bdb7f9112701a7ed49f412a0964563cb83d8
Not-Forrest <43189683+Not-Forrest@users.noreply.github.com>
2020-09-30 13:55:14 -0400

    Commit for Activity 2
    Added email field for hello.py
    - Added validators for email

--- README.md
+++ README.md
@@ -4,4 +4,22 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky
 # Activity 1
 ![task1](/screenshots/Task1.png)
 
+# Activity 2
+- Run the project and make a screenshot.
+
+![task21](/screenshots/Task21.png)
+
+- Fill in your First name and your UofT email address, click submit (make a screenshot)
+
+![task22](/screenshots/Task22.png)
+- Fill in your First name and Last name in the 1st field, and fill in your first name in the 2nd
+field, click submit. You will receive an error message (make a screenshot)
+
+![task23](/screenshots/Task23.png)
+
+- Fill in your First name and Last name in the 1st field, and fill in your NON UofT email in
+the 2nd field, click submit. (make a screenshot)
+
+![task24](/screenshots/Task24.png)
+
 
--- hello.py
+++ hello.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '122333444455555'
 
 bootstrap = Bootstrap(app)
-
 moment = Moment(app)
+
+class NameAndEmailForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -20,14 +28,28 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameAndEmailForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        session['name'] = form.name.data
+
+        if "utoronto" in form.email.data:
+            session['email'] = form.email.data
+        else:
+            session['email'] = "Please use your UofT email."
+
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), current_time=datetime.utcnow())
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
